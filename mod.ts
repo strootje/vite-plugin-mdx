5ec741d4a9edc797f8819408a48d16720576ef2c
Bastiaan Stroosnijder <bas@strootje.com>
2025-02-25 21:04:20 +0100

    init: initialized repo

---
+++ mod.ts
@@ -0,0 +1,39 @@
+import { createProcessor, ProcessorOptions } from "@mdx-js/mdx";
+import { VFile } from "vfile";
+import { Plugin } from "vinxi";
+
+export const mdx = (opts?: ProcessorOptions): Plugin[] => {
+  let processor: ReturnType<typeof createProcessor>;
+
+  return [{
+    name: "vite-plugin-mdx",
+    enforce: "pre",
+
+    configResolved({ mode }) {
+      processor = createProcessor({
+        development: mode === "development",
+        ...(opts ?? {}),
+      });
+    },
+
+    async transform(code, id) {
+      const [path] = id.split("?");
+      if (!/\.mdx?$/.test(path)) {
+        return;
+      }
+
+      if (!processor) {
+        throw Error("processor should be resolved by now..");
+      }
+
+      const result = await processor.process(
+        new VFile({ value: code, path }),
+      );
+
+      return {
+        code: result.value.toString(),
+        map: { mappings: "" },
+      };
+    },
+  }];
+};
